3daf871d470ab6bb9919351b629c223c58d37b12
Kevinp22 <Kevinpatel221999@gmail.com>
2021-07-16 18:59:47 +0530

    3 commit

---
+++ GitTestProject1/src/com/neosoft/githubtest/GitTest.java
@@ -0,0 +1,14 @@
+package com.neosoft.githubtest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		String code = "SE";
+		if(code=="SE") 
+			System.out.println("Software Enginner");
+		else
+			System.out.println(" not Software Enginner");
+
+	}
+
+}
